4a93712b003c40a9f1813d56fa199bf311ff7383
leonardneff <leonardneff@icloud.com>
2023-04-17 11:06:43 +0200

    styling edit

--- src/app/app.js
+++ src/app/app.js
@@ -1,36 +1,36 @@
 "use client" /*all components in Next 13 by default are server components, therefore for client side interactivity we need to use "use client"*/
 import { useState } from 'react';
 
-
-var count = 0;
-function Square() {
-    const [value, setValue] = useState(null);
-    function handleClick() {
-        count +=1;
-        console.log(count);
-        value === 'X' || value == null && count % 2 === 0 ? setValue('O') : setValue('X');
-    };
-
-    return (<button className="square" onClick={handleClick}> {value} </button>)
+function Square({value, onSquareClick}) {
+    return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
 export default function Board() {
+    const [squares, setSquares] = useState(Array(9).fill(null));
+
+    function handleClick(i) {
+        const nextSquares = squares.slice();
+        nextSquares[i] = 'X';
+        setSquares(nextSquares);
+        console.log(squares);
+    }
+
     return (
         <div className="board">
             <div className="board-row">
-                <Square />
-                <Square />
-                <Square />
+                <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+                <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+                <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
             </div>
             <div className="board-row">
-                <Square />
-                <Square />
-                <Square />
+                <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+                <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+                <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
             </div>
             <div className="board-row">
-                <Square />
-                <Square />
-                <Square />
+                <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+                <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+                <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
             </div>
         </div>
         
